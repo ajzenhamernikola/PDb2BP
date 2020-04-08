2765b8ef9b45336ee1a89fc5228b448eb8a88522
theikeofficial <ajzenhamernikola@gmail.com>
2020-04-08 18:44:17 +0200

    Prevedeno sve sto je vazno do 12.5 i izmenjeni su neki primeri

--- primeri/poglavlje_10/src/zadatak_10_3/Main.java
+++ primeri/poglavlje_10/src/zadatak_10_3/Main.java
@@ -7,72 +7,13 @@ class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("Pocetak rada\n");
-		
-		insertSmer();
-		deleteSmer();
+
 		insertIspitniRok();
 		
 		System.out.println("Zavrsetak rada\n");
 		HibernateUtil.getSessionFactory().close();
 	}
 
-	private static void insertSmer() {
-		Session session = HibernateUtil.getSessionFactory().openSession();
-		Smer smer = new Smer();
-		
-		smer.setId_smera(300);
-		smer.setOznaka("MATF_2019");
-		smer.setNaziv("Novi MATF smer u 2019. godini");
-		smer.setSemestara(8);
-		smer.setBodovi(240);
-		smer.setNivo(110);
-		smer.setZvanje("Diplomirani informaticar");
-		smer.setOpis("Novi smer na Matematickom fakultetu");
-		
-		Transaction TR = null;
-		try {
-			TR = session.beginTransaction();
-			
-			session.save(smer);
-			TR.commit();
-			
-			System.out.println("Smer je sacuvan");
-		} catch (Exception e) {
-			System.out.println("Cuvanje smera nije uspelo! Transakcija se ponistava!");
-			
-			if (TR != null) {
-				TR.rollback();
-			}
-		} finally {
-			session.close();
-		}
-	}
-	
-	private static void deleteSmer(){
-		Session session = HibernateUtil.getSessionFactory().openSession();
-		Smer smer = new Smer();
-		
-		Transaction TR = null;
-		try {
-			TR = session.beginTransaction();
-			
-			session.load(smer, 300); 
-			session.delete(smer);
-			
-			System.out.println("Smer obrisan");
-			
-			TR.commit();
-		} catch (Exception e) {
-			System.err.println("Brisanje smera nije uspelo! Ponistavanje transakcije!");
-			
-			if (TR != null) {
-				TR.rollback();
-			}
-		} finally {
-			session.close();
-		}	
-	}
-	
 	private static void insertIspitniRok() {
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		
