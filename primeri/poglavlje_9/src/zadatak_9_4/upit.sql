ae737d95c0efe0e14b386f4228850d7418a9fda2
djinx <key13043@gmail.com>
2020-04-11 19:25:17 +0200

    Popravljen upit i dodat u tekst

--- primeri/poglavlje_9/src/zadatak_9_4/upit.sql
+++ primeri/poglavlje_9/src/zadatak_9_4/upit.sql
@@ -6,8 +6,4 @@ WHERE  ID_PREDMETA IN (
            SELECT  ID_PREDMETA 
            FROM    OBAVEZAN_PREDMET 
            WHERE   ID_SMERA = 201 
-       ) AND 
-       ID_PREDMETA NOT IN (  
-           SELECT  ID_PREDMETA 
-           FROM    OBRADJENI_PREDMETI
        )
